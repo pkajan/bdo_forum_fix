57ad260d0d9b43170a121d39073aebb741941217
Peter Kajan <sknitro@gmail.com>
2021-02-10 12:05:21 +0100

    Create bdo_forum_fix.user.js

---
+++ bdo_forum_fix.user.js
@@ -0,0 +1,27 @@
+// ==UserScript==
+// @name         BDO forum FIX
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       pKajan
+// @updateURL    https://github.com/pkajan/YT-bdo_forum_fix/raw/master/bdo_forum_fix.user.js
+// @downloadURL  https://github.com/pkajan/YT-bdo_forum_fix/raw/master/bdo_forum_fix.user.js
+// @match        https://www.naeu.playblackdesert.com/en-US/Community?topicType=*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+var pagecode = document.getElementsByTagName('html')[0].innerHTML;
+var res = pagecode.replaceAll("<div onclick=\"location.href='/Community/Detail?topicNo=", "<div class=\"left_a\"><a style=\"font-size: 15px;\" href=\"/Community/Detail?topicNo=")
+.replaceAll("'\" class=\"left_a\">", "\"> ***LINK*** <\a>");
+res = res.replaceAll("<span class=\"label_best\">BEST</span>", "<span class=\"label_best\">B</span>")
+for (var i = 0; i < document.getElementsByClassName("ctitle").length; i++) {
+
+    console.log(document.getElementsByClassName("ctitle")[i].innerText);
+    res = res.replace("***LINK***",document.getElementsByClassName("ctitle")[i].innerText + "&nbsp;");
+
+}
+var regex = /<span class=\"ctitle\">.*<\/span>/ig;
+res = res.replaceAll(regex, "<span class=\"ctitle\"></span>")
+document.getElementsByTagName('html')[0].innerHTML = res;
+
+console.log("FIXED");
