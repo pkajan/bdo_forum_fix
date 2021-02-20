29278cf1c2bf94bca1372f2a6718a2525e16f854
Peter Kajan <sknitro@gmail.com>
2021-02-20 12:57:58 +0100

    edit box
    fixed broken (missing :D ) editbox

--- bdo_forum_fix.user.js
+++ bdo_forum_fix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BDO forum FIX
 // @namespace    http://tampermonkey.net/
-// @version      0.22
+// @version      0.24
 // @description  try to take over the world!
 // @author       pKajan
 // @updateURL    https://github.com/pkajan/YT-bdo_forum_fix/raw/master/bdo_forum_fix.user.js
@@ -11,20 +11,28 @@
 // @grant        GM_addStyle
 // ==/UserScript==
 
-var pagecode = document.getElementsByTagName('html')[0].innerHTML;
-var res = pagecode.replaceAll("<div onclick=\"location.href='/Community/Detail?topicNo=", "<div class=\"left_a\"><a style=\"font-size: 15px;\" href=\"/Community/Detail?topicNo=")
-.replaceAll("'\" class=\"left_a\">", "\"> ***LINK*** <\a>");
-res = res.replaceAll("<span class=\"label_best\">BEST</span>", "<span class=\"label_best\">B</span>")
-for (var i = 0; i < document.getElementsByClassName("ctitle").length; i++) {
 
-    console.log(document.getElementsByClassName("ctitle")[i].innerText);
-    res = res.replace("***LINK***",document.getElementsByClassName("ctitle")[i].innerText + "&nbsp;");
+if(location.href.match(/https:\/\/www\.naeu\.playblackdesert\.com\/.*-.*\/Community\/Detail\?topicNo=.*&topicType=.*/)){
+    /* TOPIC TITLE */
+    document.title = document.getElementsByClassName("title")[0].innerText;
 
+    console.log("TOPIC TITLE FIXED");
 }
-var regex = /<span class=\"ctitle\">.*<\/span>/ig;
-res = res.replaceAll(regex, "<span class=\"ctitle\"></span>")
-document.getElementsByTagName('html')[0].innerHTML = res;
+if(location.href.match(/https:\/\/www\.naeu\.playblackdesert\.com\/.*-.*\/Community\?topicType=.*/)){
+    /* TOPIC LIST */
+    var pagecode = document.getElementsByTagName('html')[0].innerHTML;
+    var res = pagecode.replaceAll("<div onclick=\"location.href='/Community/Detail?topicNo=", "<div class=\"left_a\"><a style=\"font-size: 15px;\" href=\"/Community/Detail?topicNo=")
+    .replaceAll("'\" class=\"left_a\">", "\"> ***LINK*** <\a>");
+    res = res.replaceAll("<span class=\"label_best\">BEST</span>", "<span class=\"label_best\">B</span>")
+    for (var i = 0; i < document.getElementsByClassName("ctitle").length; i++) {
 
-document.title = document.getElementsByClassName("title")[0].innerText;
+        console.log(document.getElementsByClassName("ctitle")[i].innerText);
+        res = res.replace("***LINK***",document.getElementsByClassName("ctitle")[i].innerText + "&nbsp;");
 
-console.log("FIXED");
+    }
+    var regex = /<span class=\"ctitle\">.*<\/span>/ig;
+    res = res.replaceAll(regex, "<span class=\"ctitle\"></span>")
+    document.getElementsByTagName('html')[0].innerHTML = res;
+
+    console.log("TOPIC LIST FIXED");
+}
