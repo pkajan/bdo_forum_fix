2e8383f20522d13a8d5e9939c57e4da2d5bff8d8
Peter Kajan <sknitro@gmail.com>
2021-07-22 06:49:48 +0200

    Update bdo_forum_fix.user.js

--- bdo_forum_fix.user.js
+++ bdo_forum_fix.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         BDO forum FIX
 // @namespace    http://tampermonkey.net/
-// @version      0.32
+// @version      0.33
 // @description  try to take over the world!
 // @author       pKajan
 // @updateURL    https://github.com/pkajan/bdo_forum_fix/raw/main/bdo_forum_fix.user.js
 // @downloadURL  https://github.com/pkajan/bdo_forum_fix/raw/main/bdo_forum_fix.user.js
-// @match        https://*.playblackdesert.com/*-*/Community*
+// @match        http*://*.playblackdesert.com/*-*/Forum*
 // @grant        GM_addStyle
 // ==/UserScript==
 
